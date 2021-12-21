ec2c21cae566e149c4425636d5a9c6c150a9cbdb
Yonghwi Kim <yh0921k@gmail.com>
2021-12-21 14:43:45 +0900

    add: 고양이 데이터 모킹하기

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,20 +1,14 @@
 import * as express from "express";
+import { Cat } from "./app.model";
 
 // const app: express.Application = express();
 const app: express.Express = express();
-const port: number = 8000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
-  // res.send("Hello Express");
-  // res.send({ hello: "Hello Express!" });
-  res.send({ name: "yhk", age: 30, friends: ["aaa", "bbb"] });
+  res.send({ cats: Cat });
 });
 
-app.post("/", (req: express.Request, res: express.Response) => {
-  res.send({ post: "post" });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(8000, () => {
+  console.log("server is on...");
 });
