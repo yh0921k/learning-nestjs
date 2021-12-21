230e035b942731dc4e0b1a9be082ef194a2cde7e
Yonghwi Kim <yh0921k@gmail.com>
2021-12-21 14:10:44 +0900

    add: Express + TS 개발 환경 구성 및 Hello World

---
+++ letsStart/src/app.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+
+// const app: express.Application = express();
+const app: express.Express = express();
+const port: number = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  // res.send("Hello Express");
+  // res.send({ hello: "Hello Express!" });
+  res.send({ name: "yhk", age: 30, friends: ["aaa", "bbb"] });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
