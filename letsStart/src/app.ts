39c0e9fd8d7eabdc7debffe7105168b84f95e3fd
Yonghwi Kim <yh0921k@gmail.com>
2021-12-22 13:04:10 +0900

    add: Express에서의 싱글톤 패턴 및 서비스 패턴

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,25 +1,50 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
+
 // const app: express.Application = express();
-const app: express.Express = express();
 
-/* Logging Middleware */
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("This is middleware");
-  next();
-});
+class Server {
+  public app: express.Application;
 
-// JSON Middleware
-app.use(express.json());
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-app.use(catsRouter);
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-/* 404 Middleware */
-app.use((req, res, next) => {
-  res.send({ error: "404 not found" });
-});
+  private setMiddleware() {
+    /* Logging Middleware */
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("This is middleware");
+      next();
+    });
 
-app.listen(8000, () => {
-  console.log("server is on...");
-});
+    // JSON Middleware
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    /* 404 Middleware */
+    this.app.use((req, res) => {
+      res.send({ error: "404 not found" });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log("server is on...");
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
--- letsStart/src/cats/cats.route.ts
+++ letsStart/src/cats/cats.route.ts
@@ -1,142 +1,18 @@
-import { Cat } from "./cats.model";
 import { Router } from "express";
+import {
+  createCat,
+  deleteCat,
+  readAllCat,
+  readCat,
+  updateCat,
+  updatePartialCat,
+} from "./cats.service";
 
 const router = Router();
-
-// READ 전체 데이터 조회
-router.get("/cats", (req, res) => {
-  try {
-    const cats = Cat;
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
-
-// READ 특정 데이터 조회
-router.get("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    console.log(params);
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
-
-// CREATE 새로운 데이터 추가
-router.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data);
-
-    res.status(200).send({
-      success: true,
-      data: {
-        data,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
-
-// UPDATE 데이터 전체 업데이트(PUT)
-router.put("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-
-    let result;
-    Cat.forEach((cat) => {
-      if (cat.id === params.id) {
-        cat = body;
-        result = cat;
-      }
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
-
-// UPDATE 데이터 부분 업데이트(PATCH)
-router.patch("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const body = req.body;
-
-    let result;
-    Cat.forEach((cat) => {
-      if (cat.id === params.id) {
-        cat = { ...cat, ...body };
-        result = cat;
-      }
-    });
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cat: result,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
-
-// DELETE 데이터 삭제
-router.delete("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const newCat = Cat.filter((cat) => cat.id !== params.id);
-
-    res.status(200).send({
-      success: true,
-      data: newCat,
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      data: error.message,
-    });
-  }
-});
+router.get("/cats", readAllCat);
+router.get("/cats/:id", readCat);
+router.post("/cats", createCat);
+router.put("/cats/:id", updateCat);
+router.patch("/cats/:id", updatePartialCat);
+router.delete("/cats/:id", deleteCat);
 export default router;
