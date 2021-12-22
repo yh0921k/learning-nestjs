1d03079c916af225e2c919d83456b7f4e6e0b7ee
Yonghwi Kim <yh0921k@gmail.com>
2021-12-22 09:56:05 +0900

    add: Create, Read API 개발

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,35 +4,80 @@ import { Cat } from "./app.model";
 // const app: express.Application = express();
 const app: express.Express = express();
 
+/* Logging Middleware */
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("This is middleware");
   next();
 });
 
-app.get(
-  "/cats/som",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("This is som middleware");
-    next();
-  }
-);
+// JSON Middleware
+app.use(express.json());
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+// READ 전체 데이터 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    // throw new Error("DB Connect Error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      data: error.message,
+    });
+  }
 });
 
-app.get(
-  "/cats/blue",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send({ blue: Cat[0] });
+// READ 특정 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    // throw new Error("DB Connect Error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      data: error.message,
+    });
   }
-);
-
-app.get("/cats/som", (req: express.Request, res: express.Response) => {
-  res.send({ son: Cat[1] });
 });
 
+// CREATE 새로운 데이터 추가
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data);
+
+    res.status(200).send({
+      success: true,
+      data: {
+        data,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      data: error.message,
+    });
+  }
+});
+
+/* 404 Middleware */
 app.use((req, res, next) => {
   res.send({ error: "404 not found" });
 });
