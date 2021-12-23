55078eb291235fabb9fadb751cd74e6b6e1334b9
Yonghwi Kim <yh0921k@gmail.com>
2021-12-23 13:05:57 +0900

    chore: 진행을 위한 기존 코드 제거

--- nestjs/first-project/src/app.controller.ts
+++ nestjs/first-project/src/app.controller.ts
@@ -1,23 +1,12 @@
-import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Request } from 'express';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // 데코레이터 : 함수나 클래스에 특정 기능 첨가하여 재사용성 극대화
-  // @Get() === @Get('/')
-  // localhost:8000/cats/hello
-  @Get('hello/:id/:name')
-  getHello(
-    @Req() req: Request,
-    @Body() body,
-    @Param() param: { id: string; name: string },
-  ): string {
-    // 서비스의 반환은 모듈로 들어가고 NextFactory 모듈로 이어짐
-    console.log(req);
-    console.log(param);
+  @Get()
+  getHello(): string {
     return this.appService.getHello();
   }
 }
