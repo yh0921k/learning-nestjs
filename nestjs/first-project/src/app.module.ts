9e912349845041541c0e03ce13755a57b1d32227
Yonghwi Kim <yh0921k@gmail.com>
2021-12-23 13:18:46 +0900

    add: Modules & 캡슐화

--- nestjs/first-project/src/app.module.ts
+++ nestjs/first-project/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 // Express의 Router 개념
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
