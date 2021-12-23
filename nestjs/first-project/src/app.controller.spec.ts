dc1e472b33fe4498cf411bb05e860c21b9d9e50d
Yonghwi Kim <yh0921k@gmail.com>
2021-12-23 10:37:43 +0900

    add: NestJS 프로젝트 세팅 및 Hello World

---
+++ nestjs/first-project/src/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+// 컨트롤러 테스트 목적의 소스 코드(test 패키지 또한 동일)
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
---
+++ nestjs/first-project/src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // 데코레이터 : 함수나 클래스에 특정 기능 첨가하여 재사용성 극대화
+  // @Get() === @Get('/')
+  @Get()
+  getHello(): string {
+    // 서비스의 반환은 모듈로 들어가고 NextFactory로 이어짐
+    return this.appService.getHello();
+  }
+}
