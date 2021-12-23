14d4aeefdc340b78788ed5eed3b271677b3c8d78
Yonghwi Kim <yh0921k@gmail.com>
2021-12-23 15:04:26 +0900

    add: Nest Middleware

--- nestjs/first-project/src/app.module.ts
+++ nestjs/first-project/src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './logger.middleware';
 
-// Express의 Router 개념
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
