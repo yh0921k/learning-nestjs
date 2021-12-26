81d9cb68d4322a4c3bf04cdaa691045268f4641b
Yonghwi Kim <yh0921k@gmail.com>
2021-12-26 15:44:13 +0900

    add: class-validator 파이프 등록

--- nestjs/first-project/src/main.ts
+++ nestjs/first-project/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
