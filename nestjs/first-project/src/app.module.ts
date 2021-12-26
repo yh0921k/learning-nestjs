c5d09a5d15cca2df65ae1ddfd60155fe744ab158
Yonghwi Kim <yh0921k@gmail.com>
2021-12-26 14:40:11 +0900

    add: NestJS와 DB연결 및 환경 변수 설정
    - @nestjs/config 의존성 추가 설치
    - 환경변수 파일은 Github 추적 제거되어 있음
    - MongooseModule 옵션 관련 주석은 링크 참조(https://stackoverflow.com/questions/68958221/mongoparseerror-options-usecreateindex-usefindandmodify-are-not-supported)

--- nestjs/first-project/src/app.module.ts
+++ nestjs/first-project/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true, // 동작 안함
+      //useFindAndModify: false, // 동작 안함
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
