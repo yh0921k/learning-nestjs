a46fb872de0ae9485b58ba3697b0f148ee240bab
Yonghwi Kim <yh0921k@gmail.com>
2021-12-26 15:32:31 +0900

    mod: 프로젝트에 맞게 컨트롤러 설계 변경

--- nestjs/first-project/src/cats/cats.controller.ts
+++ nestjs/first-project/src/cats/cats.controller.ts
@@ -1,20 +1,12 @@
 import {
   Controller,
-  Delete,
   Get,
-  HttpException,
-  Param,
-  ParseFloatPipe,
-  ParseIntPipe,
-  Patch,
   Post,
-  Put,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
-import { PositiveIntPipe } from '../common/pipes/positiveInt.pipe';
 import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 
 @Controller('cats')
@@ -24,37 +16,27 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  // @UseFilters(HttpExceptionFilter)
-  getAllCat() {
-    //throw new HttpException('API is broken', 401);
-
-    console.log('Hello Controller');
-    return { cats: 'get all api cat' };
+  getCurrentCat() {
+    return 'current cat';
   }
 
-  @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
-    return 'one cat';
+  @Post('')
+  async signUp() {
+    return 'signup';
   }
 
-  @Post()
-  createCat() {
-    return 'create cat';
+  @Post('login')
+  login() {
+    return 'login';
   }
 
-  @Put(':id')
-  updateCat() {
-    return 'update cat';
+  @Post('logout')
+  logout() {
+    return 'logout';
   }
 
-  @Patch(':id')
-  updatePartialCat() {
-    return ' update';
-  }
-
-  @Delete(':id')
-  deleteCat() {
-    const arr: number[] = [1, 2, 3, 4, 5];
-    return 'delete cat';
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadUmg';
   }
 }
