eed7e6aabd2a8548d985769b247e5ffb4deee151
Yonghwi Kim <yh0921k@gmail.com>
2021-12-26 23:12:33 +0900

    add: Swagger 기본 설정 구현
    - docs는 API 문서의 엔드포인트를 나타냄

--- nestjs/first-project/src/main.ts
+++ nestjs/first-project/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('CIC')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
