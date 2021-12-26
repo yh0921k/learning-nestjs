afff5ebbdfd3f929f680e8653ef7c9b1f684bff6
Yonghwi Kim <yh0921k@gmail.com>
2021-12-26 16:28:44 +0900

    add: 몽구스 모듈에 모델 등록

--- nestjs/first-project/src/cats/cats.module.ts
+++ nestjs/first-project/src/cats/cats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cats.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
   providers: [CatsService],
   exports: [CatsService],
