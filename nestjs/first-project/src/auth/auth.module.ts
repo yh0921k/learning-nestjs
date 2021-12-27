aa2d7392ee5f8775ff0181001e6add0b10f0b005
Yonghwi Kim <yh0921k@gmail.com>
2021-12-27 17:55:42 +0900

    add: JWT 서명 필드 관련 환경 변수 사용 설정

--- nestjs/first-project/src/auth/auth.module.ts
+++ nestjs/first-project/src/auth/auth.module.ts
@@ -4,12 +4,17 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { CatsModule } from '../cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     // 토큰 생성용
-    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1y' } }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1y' },
+    }),
     forwardRef(() => CatsModule), // 순환참조 방지
   ],
   providers: [AuthService, JwtStrategy],
