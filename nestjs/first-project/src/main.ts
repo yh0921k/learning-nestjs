fdc9ca0a11ba85a41619bcdfe3224e319e7bcd05
Yonghwi Kim <yh0921k@gmail.com>
2021-12-27 17:55:17 +0900

    add: Swagger API 문서 페이지 보안 관련 설정 구현

--- nestjs/first-project/src/main.ts
+++ nestjs/first-project/src/main.ts
@@ -3,11 +3,22 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.use(
+    ['/docs', 'docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('CIC')
