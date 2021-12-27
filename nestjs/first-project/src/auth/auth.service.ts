02b978e58fc5a20c83ae02f76a0130c594ac7e97
Yonghwi Kim <yh0921k@gmail.com>
2021-12-27 12:40:44 +0900

    add: 인증에 필요한 모듈 등록 및 비즈니스 로직 구현

---
+++ nestjs/first-project/src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CatsRepository } from '../cats/cats.repository';
+import { LoginRequestDto } from './dto/login.request.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly catsRepository: CatsRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async jwtLogIn(data: LoginRequestDto) {
+    const { email, password } = data;
+
+    const cat = await this.catsRepository.findCatByEmail(email);
+    if (!cat) {
+      throw new UnauthorizedException('Check your email and password');
+    }
+
+    const isPasswordValidated: boolean = await bcrypt.compare(
+      password,
+      cat.password,
+    );
+
+    if (!isPasswordValidated) {
+      throw new UnauthorizedException('Check your email and password');
+    }
+
+    const payload = { email: email, sub: cat.id };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+}
