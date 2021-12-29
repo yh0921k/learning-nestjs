a42a595f61f04c070892333b8f170f2401275cbd
Yonghwi Kim <yh0921k@gmail.com>
2021-12-29 17:52:07 +0900

    add: Config 및 Mongoose 관련 의존성 추가
    - Config 모듈 추가

--- nestjs/chattings/src/app.module.ts
+++ nestjs/chattings/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 다른 모듈에서는 ConfigModule 임포트 불필요
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
