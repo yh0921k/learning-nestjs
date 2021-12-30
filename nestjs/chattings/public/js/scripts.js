84b0bdeffef93c144d404a3c7287627bd2cf5b5e
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 11:47:33 +0900

    add: 기본적인 웹소켓 데이터 송수신 구현

--- nestjs/chattings/public/js/scripts.js
+++ nestjs/chattings/public/js/scripts.js
@@ -9,6 +9,8 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => console.log(data)); // 여기서 data는 서버 게이트웨이의 return
+  socket.on('hello_user', (data) => console.log(data)); // 추가적인 이벤트 핸들러 작업
 }
 
 function init() {
