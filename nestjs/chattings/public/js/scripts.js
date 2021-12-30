a2347ef163ced51963f97ffd1eca07e937556b09
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 11:05:45 +0900

    add: 기본적인 View 및 스크립트 작성

--- nestjs/chattings/public/js/scripts.js
+++ nestjs/chattings/public/js/scripts.js
@@ -1,1 +1,18 @@
-alert('Hello World');
+const socket = io('/');
+
+const getElementById = (id) => document.getElementById(id) || null;
+
+//* get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const username = prompt('What is your name?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
