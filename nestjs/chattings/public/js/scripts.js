0c48bf59cd04b1d9a9b8d333b6e0a9fee8039188
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 16:17:54 +0900

    add: 이벤트 설계와 기본 서비스 로직 완성

--- nestjs/chattings/public/js/scripts.js
+++ nestjs/chattings/public/js/scripts.js
@@ -9,12 +9,39 @@ const formElement = getElementById('chat_form');
 
 //* global socket handler
 socket.on('user_connected', (username) => {
-  console.log(`${username} connected!`);
+  drawNewChat(`${username} connected!`);
 });
+socket.on('new_chat', ({ chat, username }) => {
+  drawNewChat(`${username}: ${chat}`);
+});
+
+//* event callback function
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+
+    // 화면에 그리기
+    drawNewChat(`me : ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
 
 //* draw functions
 const drawHelloStranger = (username) =>
   (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+      <div>
+        ${message}
+      </div>
+      `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
 
 function helloUser() {
   const username = prompt('What is your name?');
@@ -23,6 +50,9 @@ function helloUser() {
 
 function init() {
   helloUser();
+
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
