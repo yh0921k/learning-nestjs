84033e60938e93312922a4fc53adc0502e459b23
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 09:53:02 +0900

    add: Mongoose 관련 설정 추가

--- nestjs/chattings/src/app.module.ts
+++ nestjs/chattings/src/app.module.ts
@@ -1,14 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { LoggerMiddleware } from './common/logger.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 다른 모듈에서는 ConfigModule 임포트 불필요
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
+  }
+}
