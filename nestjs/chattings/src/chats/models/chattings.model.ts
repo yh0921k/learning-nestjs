5025487e64f1dc2f4a384f6eded6409d152911e7
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 17:22:44 +0900

    add: DB 설계

---
+++ nestjs/chattings/src/chats/models/chattings.model.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+import { Socket as SocketModel } from './sockets.model';
+const options: SchemaOptions = {
+  collection: 'chattings', // 이름을 설정하지 않으면 class name을 소문자로 변환후 + 's'
+  timestamps: true,
+};
+
+@Schema(options)
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, required: true, ref: 'sockets' },
+      id: { type: String },
+      username: { type: String, required: true },
+    },
+  })
+  @IsNotEmpty()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  chat: string;
+}
+
+export const ChattingSchema = SchemaFactory.createForClass(Chatting);
