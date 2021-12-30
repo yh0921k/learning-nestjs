fda38c00837a4038352d005300f47e0b899ae4ef
Yonghwi Kim <yh0921k@gmail.com>
2021-12-30 14:04:24 +0900

    add: namespace & gateway lifecycle

--- nestjs/chattings/src/chats/chats.gateway.ts
+++ nestjs/chattings/src/chats/chats.gateway.ts
@@ -1,13 +1,38 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  afterInit(): any {
+    this.logger.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log('connect');
+    this.logger.log(socket.id);
+    this.logger.log(socket.nsp.name);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket): any {
+    this.logger.log('disconnect');
+  }
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
