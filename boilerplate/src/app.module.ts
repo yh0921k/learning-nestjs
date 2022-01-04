a7d71afae979b585dfe4318910730d12e841e721
Yonghwi Kim <yh0921k@gmail.com>
2022-01-04 10:59:55 +0900

    add: ConfigModule 설정 및 간단한 포트 변경

--- boilerplate/src/app.module.ts
+++ boilerplate/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 다른 모듈에서는 ConfigModule 임포트 불필요
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
