069d4398e0dc20e20e63a50cdfdeddf5aa502b51
Yonghwi Kim <yh0921k@gmail.com>
2022-01-04 09:21:59 +0900

    add: Health Check API 구현

--- boilerplate/src/app.controller.ts
+++ boilerplate/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('alive')
+  alive(@Res({ passthrough: true }) response: Response) {
+    response.status(200);
+    return;
   }
 }
