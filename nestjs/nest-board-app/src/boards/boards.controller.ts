019e7107ebfa4bf5330724e431bbe0585a2a5388
Yonghwi Kim <yh0921k@gmail.com>
2022-02-10 18:39:12 +0900

    add: 게시물 생성하기(Controller)

--- nestjs/nest-board-app/src/boards/boards.controller.ts
+++ nestjs/nest-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
 
@@ -12,4 +12,26 @@ export class BoardsController {
   getAllTask(): Board[] {
     return this.boardsService.getAllBoards();
   }
+
+  @Post()
+  createBoard(@Body() boardCreateRequest): Board {
+    console.log(boardCreateRequest.title);
+    console.log(boardCreateRequest.description);
+
+    return this.boardsService.createBoard(
+      boardCreateRequest.title,
+      boardCreateRequest.description,
+    );
+  }
+
+  // @Post()
+  // createBoard(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Board {
+  //   console.log(title);
+  //   console.log(description);
+  //
+  //   return this.boardsService.createBoard(title, description);
+  // }
 }
