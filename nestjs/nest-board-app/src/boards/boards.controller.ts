9fa5d6a1f34d913f6932d3587ea3cdbc7f232131
Yonghwi Kim <yh0921k@gmail.com>
2022-02-10 17:40:21 +0900

    add: 모든 게시물을 가져오는 Service 만들기

--- nestjs/nest-board-app/src/boards/boards.controller.ts
+++ nestjs/nest-board-app/src/boards/boards.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {
     this.boardsService = boardsService;
+  }
+
+  @Get()
+  getAllTask() {
+    return this.boardsService.getAllBoards();
   }
 }
