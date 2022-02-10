4d89cd9da74dbe81ebca112e89453318f17de306
Yonghwi Kim <yh0921k@gmail.com>
2022-02-10 18:22:08 +0900

    add: 게시물 생성하기(Service)

--- nestjs/nest-board-app/src/boards/boards.service.ts
+++ nestjs/nest-board-app/src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,19 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title: title,
+
+      // 이와 같이 프로퍼티명을 명시하지 않으면 파라미터의 이름과 동일한 프로퍼티가 생성된다.
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
