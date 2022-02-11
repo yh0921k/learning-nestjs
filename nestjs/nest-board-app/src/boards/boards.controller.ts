6ece8eaca06ad1d923dec135c23a7b3d664e36a4
Yonghwi Kim <yh0921k@gmail.com>
2022-02-11 11:33:14 +0900

    add: id로 특정 게시물 지우기

--- nestjs/nest-board-app/src/boards/boards.controller.ts
+++ nestjs/nest-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -23,7 +23,12 @@ export class BoardsController {
   }
 
   @Get(':id')
-  getBoardById(@Param('id') id: string) {
+  getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete(':id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
