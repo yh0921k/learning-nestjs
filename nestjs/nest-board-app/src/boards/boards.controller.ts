8611d41299c2192ce16c61914fcd27b96db8dafa
Yonghwi Kim <yh0921k@gmail.com>
2022-02-12 10:54:51 +0900

    add: 커스텀 파이프를 이용한 유효성 체크 구현

--- nestjs/nest-board-app/src/boards/boards.controller.ts
+++ nestjs/nest-board-app/src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './boards.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -46,7 +47,7 @@ export class BoardsController {
   @Patch(':id')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Board {
     return this.boardsService.updateBoardStatus(id, status);
   }
