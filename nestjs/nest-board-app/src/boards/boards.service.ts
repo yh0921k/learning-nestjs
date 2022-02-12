03afac6c75cce1621d78b55e4d322e55f6028a3a
Yonghwi Kim <yh0921k@gmail.com>
2022-02-12 10:18:36 +0900

    add: 특정 게시물을 찾을 때 없는 경우 예외 처리

--- nestjs/nest-board-app/src/boards/boards.service.ts
+++ nestjs/nest-board-app/src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -25,7 +25,13 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException('게시글을 찾을 수 없습니다.');
+    }
+
+    return found;
   }
 
   deleteBoard(id: string): void {
