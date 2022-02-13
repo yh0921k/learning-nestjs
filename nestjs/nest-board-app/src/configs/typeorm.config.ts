aa7654e7a05d89f30600208e113e7d1e9252ff9f
Yonghwi Kim <yh0921k@gmail.com>
2022-02-13 16:05:16 +0900

    add: TypeORM 어플리케이션에서 사용하기

---
+++ nestjs/nest-board-app/src/configs/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: '218.155.184.17',
+  port: 13306,
+  username: 'blockodyssey',
+  password: '1234',
+  database: 'board',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // set 'false' in production
+  autoLoadEntities: true,
+  logging: true, // set 'false' in production
+  keepConnectionAlive: true,
+};
