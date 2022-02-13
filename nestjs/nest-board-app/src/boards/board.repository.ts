4d0688552790b5fd6a4af224901625daf03789cb
Yonghwi Kim <yh0921k@gmail.com>
2022-02-13 16:59:35 +0900

    add: Repository 구현 및 설정

---
+++ nestjs/nest-board-app/src/boards/board.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { BoardEntity } from './board.entity';
+
+@EntityRepository(BoardEntity)
+export class BoardRepository extends Repository<BoardEntity> {}
