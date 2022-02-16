06eb4b1b34b399e3f9327d2f32d559612bae0e55
Yonghwi Kim <yh0921k@gmail.com>
2022-02-16 20:36:29 +0900

    add: AuthModule 관련 컨트롤러 및 서비스 레이어 기본 코드 구현

--- nestjs/nest-board-app/src/app.module.ts
+++ nestjs/nest-board-app/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './boards/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardModule, TypeOrmModule.forRoot(typeormConfig)],
+  imports: [BoardModule, AuthModule, TypeOrmModule.forRoot(typeormConfig)],
 })
 export class AppModule {}
