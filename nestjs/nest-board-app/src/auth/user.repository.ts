2e54bc3767b70b2c6ac1779ee8724befbd6e8662
Yonghwi Kim <yh0921k@gmail.com>
2022-02-16 21:06:20 +0900

    add: 사용자 중복 처리를 위한 유니크 제약조건 설정

--- nestjs/nest-board-app/src/auth/user.repository.ts
+++ nestjs/nest-board-app/src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(UserEntity)
 export class UserRepository extends Repository<UserEntity> {
@@ -8,6 +12,15 @@ export class UserRepository extends Repository<UserEntity> {
     const { username, password } = authCredentialDto;
 
     const user = await this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.errno === 1062) {
+        throw new ConflictException('해당 이름은 이미 사용중입니다.');
+      } else {
+        throw new InternalServerErrorException('백엔드 개발자에게 문의주세요.');
+      }
+    }
   }
 }
