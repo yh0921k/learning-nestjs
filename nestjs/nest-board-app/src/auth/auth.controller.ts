b2cac84725d7f59529e954855b10020819da638b
Yonghwi Kim <yh0921k@gmail.com>
2022-02-16 20:51:07 +0900

    add: 사용자 회원가입 기능 구현

--- nestjs/nest-board-app/src/auth/auth.controller.ts
+++ nestjs/nest-board-app/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth.credential.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+    return await this.authService.signUp(authCredentialDto);
+  }
+}
--- nestjs/nest-board-app/src/auth/user.repository.ts
+++ nestjs/nest-board-app/src/auth/user.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { AuthCredentialDto } from './dto/auth.credential.dto';
 
 @EntityRepository(UserEntity)
-export class UserRepository extends Repository<UserEntity> {}
+export class UserRepository extends Repository<UserEntity> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+
+    const user = await this.create({ username, password });
+    await this.save(user);
+  }
+}
