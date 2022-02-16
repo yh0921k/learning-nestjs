6e2e5eabcfa74003c2b239b9119d14325f42ce47
Yonghwi Kim <yh0921k@gmail.com>
2022-02-16 20:56:26 +0900

    add: 사용자 데이터 유효성 검사 로직 추가 구현

--- nestjs/nest-board-app/src/auth/auth.controller.ts
+++ nestjs/nest-board-app/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
 
@@ -7,7 +7,9 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+  async signUp(
+    @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
+  ): Promise<void> {
     return await this.authService.signUp(authCredentialDto);
   }
 }
