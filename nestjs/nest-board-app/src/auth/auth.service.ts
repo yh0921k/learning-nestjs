7f0298f4ac565d4ef5cd482ff8dec41f2717fff8
Yonghwi Kim <yh0921k@gmail.com>
2022-02-18 15:46:01 +0900

    add: JWT를 이용한 토큰 생성

--- nestjs/nest-board-app/src/auth/auth.service.ts
+++ nestjs/nest-board-app/src/auth/auth.service.ts
@@ -3,23 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return await this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'Sign In Success';
+      const payload = { username };
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Sign In failed');
     }
