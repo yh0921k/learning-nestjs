9ae006c14f135d3fb2d58ac1ee9554003cb9d29b
Yonghwi Kim <yh0921k@gmail.com>
2022-02-19 10:23:18 +0900

    add: AuthGuard 테스트

--- nestjs/nest-board-app/src/auth/auth.controller.ts
+++ nestjs/nest-board-app/src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +27,11 @@ export class AuthController {
     @Body(ValidationPipe) authCredentialDto: AuthCredentialDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialDto);
+  }
+
+  @Get('jwt-test')
+  @UseGuards(AuthGuard())
+  jwtTest(@Req() req) {
+    console.log(req);
   }
 }
