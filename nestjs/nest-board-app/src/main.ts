74377240c681bed4d4b41e442d367d5f64e58659
Yonghwi Kim <yh0921k@gmail.com>
2022-02-20 11:31:57 +0900

    add: 설정 파일을 이용한 서버 포트 사용

--- nestjs/nest-board-app/src/main.ts
+++ nestjs/nest-board-app/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
   const logger = new Logger('Main');
-  const port = 3000;
   await app.listen(port);
 
   logger.log(`Application Running on Port ${port}`);
